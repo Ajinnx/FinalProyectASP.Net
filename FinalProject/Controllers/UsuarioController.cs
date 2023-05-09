46bb89130fafa468e6667d8a9353a72203d622fa
Iván Moreno <ivanmoreno1312@gmail.com>
2023-05-09 16:33:09 +0200

    Añadido UsuarioController, que se ocupa de acciones genéricas para todos los tipos de usuarios.

---
+++ FinalProject/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using FinalProject.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers;
+
+[Route("api/usuario")]
+[ApiController]
+public sealed class UsuarioController : ControllerBase
+{
+	private readonly AppDbContext _context;
+
+	public UsuarioController(AppDbContext context)
+	{
+		_context = context;
+	}
+
+	[HttpGet("{nombreUsuario}")]
+	public IActionResult Get(string nombreUsuario)
+	{
+		object? encontrado = _context.UsuariosReceptores.Where(usuario => usuario.Usuario == nombreUsuario).FirstOrDefault();
+		encontrado ??= _context.UsuariosProveedores.Where(usuario => usuario.Usuario == nombreUsuario).FirstOrDefault();
+		encontrado ??= _context.UsuariosEmpresas.Where(usuario => usuario.Usuario == nombreUsuario).FirstOrDefault();
+		encontrado ??= _context.UsuariosOngs.Where(usuario => usuario.Usuario == nombreUsuario).FirstOrDefault();
+
+		return encontrado is null
+			? NotFound()
+			: Ok(encontrado);
+	}
+}
